a03a859cf4fc399abe452878011b4893796991ed
kiddieverts <kiddieverts@gmail.com>
2021-05-28 22:58:02 +0000

    cleanup

--- Domain/Infrastructure/AggregateRoot.cs
+++ Domain/Infrastructure/AggregateRoot.cs
@@ -6,16 +6,15 @@ namespace MyRental
 {
     public abstract class AggregateRoot
     {
+        public Guid Id { get; protected set; }
+        public long Version { get; protected set; } = InitalVersion;
+        protected static long InitalVersion => -1;
+        private readonly ICollection<IDomainEvent> _uncommittedEvents = new LinkedList<IDomainEvent>();
+
         public AggregateRoot(long version)
         {
             Version = version;
         }
-
-        public Guid Id { get; protected set; }
-        public long Version { get; protected set; } = InitalVersion;
-        protected static long InitalVersion => -1;
-
-        private readonly ICollection<IDomainEvent> _uncommittedEvents = new LinkedList<IDomainEvent>();
 
         public void ClearUncommittedEvents() => _uncommittedEvents.Clear();
 
