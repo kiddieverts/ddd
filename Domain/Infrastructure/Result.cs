025a6f7d56015f89f911def3d852ad23b784c29f
kiddieverts <kiddieverts@gmail.com>
2021-05-29 20:40:24 +0000

    Add result object

---
+++ Domain/Infrastructure/Result.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace MyRental
+{
+    public record Result<T>
+    {
+        public bool IsSuccess { get; init; }
+
+        public T Value;
+        public List<string> Errors { get; init; } = new List<string>();
+
+        private Result() { }
+
+        public static Result<T> Succeed(T value)
+        {
+            return new Result<T>
+            {
+                IsSuccess = true,
+                Value = value
+            };
+        }
+        public static Result<T> Failure(string err)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                Errors = new List<string> { err }
+            };
+        }
+    }
+
+}
