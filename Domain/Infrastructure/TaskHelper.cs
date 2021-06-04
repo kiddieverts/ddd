94332a96a404eb34fa533f3e66277a803bc6a07f
kiddieverts <kiddieverts@gmail.com>
2021-06-04 16:18:49 +0000

    Refactor

--- Domain/Infrastructure/TaskHelper.cs
+++ Domain/Infrastructure/TaskHelper.cs
@@ -5,11 +5,8 @@ namespace MyRental
 {
     public static class TaskHelper
     {
-        public static async Task<Result<T>> SelectMany<R, T>(this Task<Result<R>> result, Func<R, Task<Result<T>>> f)
-        {
-            var r = await result;
-            return await r.SelectMany(f);
-        }
+        public static async Task<Result<T>> SelectMany<R, T>(this Task<Result<R>> result, Func<R, Task<Result<T>>> f) =>
+            await result.SelectMany(f);
 
         public static Task<Result<T>> SelectMany<T>(this Task<Result<T>> result, Func<T, Result<T>> f) => result;
     }
